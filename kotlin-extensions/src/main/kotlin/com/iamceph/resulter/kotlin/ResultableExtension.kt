880c8863bcd3991985a6c899991e645c16780da5
zlataovce <34477304+zlataovce@users.noreply.github.com>
2022-02-26 10:40:54 +0100

    feat & refactor: more kotlin extensions

--- kotlin-extensions/src/main/kotlin/com/iamceph/resulter/kotlin/ResultableExtension.kt
+++ kotlin-extensions/src/main/kotlin/com/iamceph/resulter/kotlin/ResultableExtension.kt
@@ -3,34 +3,32 @@ package com.iamceph.resulter.kotlin
 import com.iamceph.resulter.core.Resultable
 import com.iamceph.resulter.core.model.ProtoResultable
 import reactor.core.publisher.Mono
-import java.util.function.Consumer
 
 /**
  * Converts [ProtoResultable] to [Resultable].
  */
-fun ProtoResultable.resultable(): Resultable {
-    return Resultable.convert(this)
-}
+fun ProtoResultable.resultable(): Resultable = Resultable.convert(this)
 
 /**
  * Converts [ProtoResultable] to [Resultable].
  */
-fun Mono<ProtoResultable>.resultable(): Mono<Resultable> {
-    return this.map { it.resultable() }
-}
+fun Mono<ProtoResultable>.resultable(): Mono<Resultable> = map { it.resultable() }
 
-fun Mono<Resultable>.ifOk(resultable: Consumer<Resultable>): Mono<Resultable> {
-    return this.doOnNext {
-        if (it.isOk) {
-            resultable.accept(it)
-        }
+fun Mono<Resultable>.ifOk(resultable: (Resultable) -> Unit): Mono<Resultable> = doOnNext {
+    if (it.isOk) {
+        resultable(it)
     }
 }
 
-fun Mono<Resultable>.ifFail(resultable: Consumer<Resultable>): Mono<Resultable> {
-    return this.doOnNext {
-        if (it.isFail) {
-            resultable.accept(it)
-        }
+fun Mono<Resultable>.ifFail(resultable: (Resultable) -> Unit): Mono<Resultable> = doOnNext {
+    if (it.isFail) {
+        resultable(it)
     }
+}
+
+inline fun resultable(block: () -> Unit): Resultable = try {
+    block()
+    Resultable.ok()
+} catch (e: Exception) {
+    Resultable.fail(e)
 }
