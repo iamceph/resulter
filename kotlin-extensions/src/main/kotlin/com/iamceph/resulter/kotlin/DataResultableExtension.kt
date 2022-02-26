4c4d4679bd272fd709e30e9ed7482452174084e7
Matouš Kučera <34477304+zlataovce@users.noreply.github.com>
2022-02-26 10:52:48 +0100

    feat & refactor: more kotlin extensions (#4)

--- kotlin-extensions/src/main/kotlin/com/iamceph/resulter/kotlin/DataResultableExtension.kt
+++ kotlin-extensions/src/main/kotlin/com/iamceph/resulter/kotlin/DataResultableExtension.kt
@@ -5,28 +5,20 @@ import com.iamceph.resulter.core.DataResultable
 import com.iamceph.resulter.core.model.ResultableData
 import com.iamceph.resulter.core.pack.Packeto
 import reactor.core.publisher.Mono
-import java.util.function.Consumer
-import java.util.function.Supplier
 import kotlin.reflect.KClass
 
-fun <T> DataResultable<T>.pack(): ResultableData {
-    return Packeto.pack(this)
-}
+fun <T> DataResultable<T>.pack(): ResultableData = Packeto.pack(this)
 
-fun <T> Mono<DataResultable<T>>.pack(): Mono<ResultableData> {
-    return this.map { it.pack() }
-}
+fun <T> Mono<DataResultable<T>>.pack(): Mono<ResultableData> = map { it.pack() }
 
-fun <T> Mono<DataResultable<T>>.ifOk(data : Consumer<T>) : Mono<DataResultable<T>> {
-    return this.doOnNext{
-        it.ifOk(data)
-    }
-}
+fun <T> Mono<DataResultable<T>>.ifOk(data: (T) -> Unit): Mono<DataResultable<T>> = doOnNext { it.ifOk(data) }
 
-fun <K : Message> ResultableData.unpack(type: KClass<K>): DataResultable<K> {
-    return Packeto.unpack(this.data, type.java)
-}
+fun <K : Message> ResultableData.unpack(type: KClass<K>): DataResultable<K> = Packeto.unpack(data, type.java)
 
-fun <K : Message> Mono<ResultableData>.unpack(type: KClass<K>): Mono<DataResultable<K>> {
-    return this.map { it.unpack(type) }
+fun <K : Message> Mono<ResultableData>.unpack(type: KClass<K>): Mono<DataResultable<K>> = map { it.unpack(type) }
+
+inline fun <T> dataResultable(block: () -> T?): DataResultable<T> = try {
+    DataResultable.failIfNull(block())
+} catch (e: Exception) {
+    DataResultable.fail(e)
 }
